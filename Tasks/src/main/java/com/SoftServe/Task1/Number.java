b94e52097482cf3f358facd6f42a03d1b2567e07
Zerref1 <vlad1234500@gmail.com>
2022-01-20 17:32:34 +0200

    Task1

--- Tasks/src/main/java/com/SoftServe/Task1/Number.java
+++ Tasks/src/main/java/com/SoftServe/Task1/Number.java
@@ -1,15 +1,14 @@
 package com.SoftServe.Task1;
 
-import java.util.Scanner;
-
 class Number {
     private int n;
-    Number(int n){
+
+    Number(int n) {
         this.n = n;
     }
 
-    public int[] SplitNumber(){
-        int [] arrayOfNumbers = new int[3];
+    private int[] SplitNumber() {
+        int[] arrayOfNumbers = new int[3];
         int num = n;
         int[] b = new int[3];
         for (int i = 0; i < 3; i++) {
@@ -18,10 +17,11 @@ class Number {
         }
         return arrayOfNumbers;
     }
-    public void PitCheck(){
+
+    public void PitCheck() {
         int[] arrayOfNumbers = SplitNumber();
-        if(arrayOfNumbers[0]>arrayOfNumbers[1]&&arrayOfNumbers[2]>arrayOfNumbers[1]){
+        if (arrayOfNumbers[0] > arrayOfNumbers[1] && arrayOfNumbers[2] > arrayOfNumbers[1]) {
             System.out.println("This number is a gap!");
-        }else System.out.println("This number isn't a gap!");
+        } else System.out.println("This number isn't a gap!");
     }
-    }
+}
